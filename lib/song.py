f5c9fee5687fde03a74c34266602c35ddb938c57
Rodney_Karani <rodneykarani07@gmail.com>
2025-08-20 10:29:02 +0300

    Correct lab

--- lib/song.py
+++ lib/song.py
@@ -3,8 +3,8 @@ class Song:
     count = 0
     genres = []
     artists = []
-    genres_count = {}
-    artists_count = {}
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -14,12 +14,12 @@ class Song:
         self.add_song_to_count()
         self.add_to_genres(genre)
         self.add_to_artists(artist)
-        self.add_to_genres_count(genre)
-        self.add_to_artists_count(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
 
-        @classmethod
-        def add_song_to_count(cls):
-            cls.count += 1
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
 
     @classmethod
     def add_to_genres(cls, genre):
@@ -32,16 +32,16 @@ class Song:
             cls.artists.append(artist)
 
     @classmethod
-    def add_to_genres_count(cls, genre):
-        if genre in cls.genres_count:
-            cls.genres_count[genre] += 1
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
         else:
-            cls.genres_count[genre] = 1
+            cls.genre_count[genre] = 1
 
     @classmethod
-    def add_to_artists_count(cls, artist):
-        if artist in cls.artists_count:
-            cls.artists_count[artist] += 1
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
         else:
-            cls.artists_count[artist] = 1
+         cls.artist_count[artist] = 1
 
